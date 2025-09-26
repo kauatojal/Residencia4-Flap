35f4e90e8b4f2276fcdeba97b6b317775dcebeec
appositum <appositum@pm.me>
2025-09-26 08:18:22 -0300

    Adicionar validação para formato de email no modelo de usuário

--- backend/src/main/java/com/flap/backend/models/User.java
+++ backend/src/main/java/com/flap/backend/models/User.java
@@ -1,6 +1,7 @@
 package com.flap.backend.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -15,6 +16,7 @@ public class User {
     private String name;
 
     @NotBlank
+    @Email
     @Column(unique = true, nullable = false)
     private String email;
 
