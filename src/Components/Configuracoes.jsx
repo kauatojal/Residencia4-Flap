66cbebf9b2d622fd40aeb5706530ad7b40ad9d85
Kauã Tojal <150505239+kauatojal@users.noreply.github.com>
2025-10-09 23:50:52 -0300

    Delete src directory

--- src/Components/Configuracoes.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function Configuracoes() {
-  return <div style={{padding:40}}><h2>Configurações</h2></div>;
-}
--- src/Components/Notificacoes.jsx
+++
@@ -1,4 +0,0 @@
-import React from "react";
-export default function Notificacoes() {
-  return <div style={{padding:40}}><h2>Notificações</h2></div>;
-}
