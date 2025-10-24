2ea38bfc157566efa721170f16aa1956308e64de
Kauã Tojal <150505239+kauatojal@users.noreply.github.com>
2025-10-24 11:22:41 -0300

    Delete backend directory

--- backend/src/main/java/com/flap/backend/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flap.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.flap.backend.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
